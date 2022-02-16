42be0e601fb099912e5a1e3d26161e5b6293db72
sagar-wallflower <99609561+sagar-wallflower@users.noreply.github.com>
2022-02-16 19:53:13 +0530

    Create designs.java

---
+++ designs.java
@@ -0,0 +1,29 @@
+public static void main(String args[])
+	{
+		System.out.println("Hello World");
+		
+	    
+		
+		for(int i=0;i<5;i++)
+		{
+			System.out.print("$ ");
+		}
+		System.out.println();
+		
+		
+		for(int i=0;i<2;i++)
+		{
+			for(int j=0;j<2;j++)
+			{
+				System.out.print("$       ");
+			}
+		System.out.println();	
+		}	
+		
+	
+		for(int i=0;i<5;i++)
+		{
+			System.out.print("$ ");
+		}
+		
+	}
