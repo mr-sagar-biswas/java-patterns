c05f42d9fd9e68a7c4502d3aaeb0c2e3c543ded3
sagar-wallflower <99609561+sagar-wallflower@users.noreply.github.com>
2022-02-16 20:31:52 +0530

    Create designs2.java

---
+++ designs2.java
@@ -0,0 +1,36 @@
+package workingjava;
+
+public class designs2
+{
+	public static void main(String args[])
+	{
+		System.out.println("Hello World");
+		/*       * 
+			      * * 
+			     * * * 
+			    * * * * 
+			   * * * * * 
+		    * * * * * *      */
+		
+		int p=6;
+		for(int j=1;j<7;j++)
+		{
+			for(int i=0;i<p;i++)
+			{
+				System.out.print(" ");
+			}
+			
+			
+			for(int i = 0;i<j;i++)
+			{
+				System.out.print("* ");
+			}
+			
+		p--;
+		
+		System.out.println();
+		}
+	
+		
+	}
+}
