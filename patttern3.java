43db36ea17d5453b48a0ba92a73a0edc84f90d86
sagar-wallflower <99609561+sagar-wallflower@users.noreply.github.com>
2022-02-16 20:46:00 +0530

    Create patttern3.java

---
+++ patttern3.java
@@ -0,0 +1,68 @@
+package workingjava;
+
+public class pattern3
+{
+	public static void main(String args[])
+	{
+		System.out.println("Hello World");
+		/*  
+		    
+	    *            
+     * * 
+    * * * 
+   * * * * 
+  * * * * * 
+ * * * * * * 
+ * * * * * * 
+  * * * * * 
+   * * * * 
+    * * * 
+     * * 
+      * 
+ */
+		
+		int p=6;
+		for(int j=1;j<7;j++)
+		{
+			for(int i=0;i<p;i++)
+			{
+				System.out.print(" ");
+			}
+			
+			
+			for(int i = 0;i<j;i++)
+			{
+				System.out.print("* ");
+			}
+			
+		p--;
+		
+		System.out.println();
+		}
+		
+		
+		int t=1;
+		for(int j=6;j>0;j--)
+		{
+			
+			for(int i = 0;i<t;i++)
+			{
+				System.out.print(" ");
+			}
+			
+			
+			
+			
+			
+			
+			for(int i=0;i<j;i++)
+			{
+				System.out.print("* ");
+			}
+		System.out.println();
+		t++;
+		}
+	
+		
+	}
+}
