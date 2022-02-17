4eecaf04a07cdd76fc5aaf3762f643d6ca74baf4
SAGAR BISWAS <99609561+sagar-wallflower@users.noreply.github.com>
2022-02-17 12:02:41 +0530

    Create diamond01.java

---
+++ diamond01.java
@@ -0,0 +1,105 @@
+package workingjava;
+
+public class firstcode
+{
+	public static void main(String args[])
+	{
+		System.out.println("Hello World");
+		System.out.println();
+		/*  
+		    
+          **
+         *  *
+        *    *
+       *      *
+      *        *
+     *          *
+    *            *
+   *              *
+  *                *
+ *                  *
+ *                  *
+  *                *
+   *              *
+    *            *
+     *          *
+      *        *
+       *      *
+        *    *
+         *  *
+          **
+
+
+
+
+ */
+		
+			
+	
+		
+		
+		
+		int x=10;
+		int y=0;
+		for(int i =0;i<10;i++)
+		{
+			
+			for(int j=0;j<x;j++)
+			{
+				System.out.print(" ");
+			}
+			
+			
+			
+			
+			System.out.print("*");
+			
+			for(int j=y;j>0;j--)
+			{
+				System.out.print(" ");
+			}
+			
+			
+			
+			System.out.print("*");
+			System.out.println();
+			x--;
+			y+=2;
+		}
+		
+		
+		int p=1;
+		int q=18;
+		for(int i =0;i<10;i++)
+		{
+			
+			for(int j=0;j<p;j++)
+			{
+				System.out.print(" ");
+			}
+			
+			
+			
+			
+			System.out.print("*");
+			
+			for(int j=q;j>0;j--)
+			{
+				System.out.print(" ");
+			}
+			
+			
+			
+			System.out.print("*");
+			System.out.println();
+			p++;
+			q-=2;
+		}
+		
+		
+		
+		
+	
+		
+	}
+}
